28f4d55c0dd469a511e12df8e316a1cd0c05ccda
Lhayla Shamy Santos Fonseca <160618204+Lhayla-Shamy1@users.noreply.github.com>
2025-03-25 16:46:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,61 @@
+let dados = [
+    {   nome : "Lamen coreano (라면)",
+        descricao : "Macarrão instantâneo com sabores picantes e variados, como kimchi e frutos do mar. É uma refeição rápida e saborosa, perfeita para quem busca praticidade.",
+        link : "https://www.hachi8.com.br/alimentos/lamen-coreano-super-picante-buldak-kimchi-big-bowl-samyang-105g",
+        img: "img/lamen.png"
+    },
+    {
+        nome : "Topokki (떡볶이)",
+        descricao : "Bolinhos de arroz cilíndricos cozidos em um molho picante e adocicado à base de gochujang (pasta de pimenta coreana). É um prato popular de rua na Coreia do Sul.",
+        link : "https://comidasasiaticas.com/tteokbokki-topokki-como-fazer-prato-coreano/",
+        img: "img/tteokbokki.png"       
+    },
+    {
+        nome : "Kimchi (김치)",
+        descricao : " Conserva de vegetais fermentados, geralmente acelga e nabo, com temperos picantes. É um acompanhamento essencial na culinária coreana, rico em probióticos e sabor único.",
+        link : "https://www.bbc.com/portuguese/articles/cnerglzzmpno#:~:text=Para%20preparar%20o%20kimchi%2C%20a,ambiente%20e%20depois%20na%20geladeira.",
+        img:"img/kimchi.png"
+    },
+    {
+        nome: "Haemul Pajeon (해물파전)",
+        descricao: "Panqueca de frutos do mar com cebolinha, lula, camarão e outros frutos do mar. É uma ótima opção para compartilhar e combina bem com makgeolli (vinho de arroz coreano).",
+        link: "https://www.maangchi.com/recipe/haemul-pajeon",
+        img:"img/haemulpajeon.png"
+    },
+    {
+        nome: "Galbi (갈비)",
+        descricao: "Costelas bovinas ou suínas marinadas em molho de soja, alho, gengibre e outros temperos, grelhadas na mesa. É um prato popular em churrascos coreanos.",
+        link: "https://www.mykoreankitchen.com/galbi-korean-bbq-beef-short-ribs/",
+        img:"img/galbi.png"
+    },
+    {
+        nome: "Sundubu Jjigae (순두부찌개)",
+        descricao: "Ensopado picante de tofu macio, carne (geralmente bovina ou frutos do mar), vegetais e gochujang. É uma sopa reconfortante e popular, especialmente no inverno.",
+        link: "https://www.koreanbapsang.com/soondubu-jjigae-soft-tofu-stew/",
+        img:"img/sundubu.png"
+    },
+    {
+        nome: "Kimchi Jjigae (김치찌개)",
+        descricao: "Ensopado picante feito com kimchi fermentado, carne de porco, tofu e vegetais. É um prato clássico da culinária coreana, saboroso e reconfortante.",
+        link: "https://www.maangchi.com/recipe/kimchi-jjigae",
+        img:"img/Kimchi-jjiga.png"
+    },
+    {
+        nome: "Bossam (보쌈)",
+        "descricao": "Barriga de porco cozida no vapor, servida com kimchi, alho, pimenta e outros acompanhamentos. É uma refeição social e saborosa, geralmente consumida enrolada em folhas de alface ou repolho.",
+        link: "https://www.mykoreankitchen.com/bossam-korean-boiled-pork-wraps/",
+        img:"img/bossamm.png"
+    },
+    {
+        nome: "Yukgaejang (육개장)",
+        descricao: "Sopa picante de carne bovina desfiada, cebolinha, broto de samambaia e outros vegetais. É uma sopa rica e saborosa, ideal para os dias frios.",
+        link: "https://www.maangchi.com/recipe/yukgaejang",
+        img:"img/Yukgaejang.png"
+    },
+    {
+        nome: "Patbingsu (팥빙수)",
+        descricao: "Sobremesa gelada feita com gelo raspado, pasta de feijão doce, leite condensado, frutas e outros ingredientes. É uma sobremesa refrescante e popular, especialmente no verão.",
+        link: "https://www.mykoreankitchen.com/patbingsu/",
+        img:"img/agoravai.png"
+    }
+];
